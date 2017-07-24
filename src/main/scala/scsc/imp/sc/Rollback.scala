55c191c11c1f02a5cbc07b4291334d905e171d7f
Nate Nystrom <nate.nystrom@usi.ch>
2017-07-25 00:57:50 +0200

    Implemented split functionality for imp.sc.

---
+++ src/main/scala/scsc/imp/sc/Rollback.scala
@@ -0,0 +1,76 @@
+package scsc.imp.sc
+
+import scala.collection.mutable.ListBuffer
+
+class Rollback[M <: Machine](val machine: M) {
+  import machine._
+  import terms._
+  import states._
+  import continuations._
+  import envs._
+  import stores._
+
+  // Rollback converts an active state to a residual state.
+  // This happens either when splitting fails or when
+  // the (meta) whistle blows, preventing a split.
+  // Because some states cannot be residualized, it may
+  // actually rollback the history. This is why it takes
+  // a history and returns a new history.
+  // Rollback can fail if the initial state of the history
+  // is a split state. In this case, the split fails entirely
+  // and the root of the split has to be rolled back.
+  // In the worst case we rollback the entire history
+  // to the initial state and residualize that.
+  // This is safe, but clearly not desirable: a lot of work
+  // for no gain.
+  def rollback(hist: List[State]): Option[List[State]] = hist match {
+    case Ev(e, ρ, σ, k)::tail =>
+      Some(rebuildEv(e, ρ, σ, k)::tail)
+
+    case Co(v, σ, k)::tail =>
+      v match {
+        case Path(loc, path) =>
+          // cannot safely residualize a location
+          tail match {
+            // if this is the last state in the history, fail.
+            case Nil => None
+            case tail => rollback(tail)
+          }
+        case v =>
+          Some(rebuildCo(v, σ, k)::tail)
+      }
+
+    case Unwinding(j, σ, k)::tail =>
+      // OK, this is a problem too.
+      // We need to ensure the landing pad is in
+      // the continuation before rolling back
+      // and ensure that ReStep reconstructs
+      // the landing pad.
+      // This prevents break and continue from
+      // appearing outside a loop or return outside
+      // a function body.
+      Some(rebuildUnwinding(j, σ, k)::tail)
+
+    case _ =>
+      None
+  }
+
+  def rebuildEv(e: Exp, ρ: Env, σ: Store, k: Cont): Re = e match {
+    case For(label, Empty(), test, Empty(), body) =>
+      rebuildEv(While(label, test, body), ρ, σ, k)
+
+    case e =>
+      println(s"simulating $e")
+      println(s"initial σ = $σ")
+      println(s"  final σ = ${stores.simulateStore(e)(σ, ρ)}")
+      Re(e, stores.simulateStore(e)(σ, ρ), k)
+  }
+
+  def rebuildCo(e: Value, σ: Store, k: Cont): Re = {
+    Re(e, σ, k)
+  }
+
+  def rebuildUnwinding(e: Jump, σ: Store, k: Cont): Re = {
+    Re(e, σ, k)
+  }
+}
--- src/main/scala/scsc/imp/sc/Stores.scala
+++ src/main/scala/scsc/imp/sc/Stores.scala
@@ -4,4 +4,16 @@ import scsc.imp
 
 trait Stores extends imp.Stores with scsc.sc.Stores {
   type MachineType <: Machine { type StoresType = Stores.this.type }
+
+  import machine._
+  import terms._
+  import envs._
+  import stores._
+
+  // TODO:
+  // At this point, we run the machine in abstract interpretation mode
+  // to simulate the store. See the AAM work of Van Horn and Might.
+  def simulateStore(e: Exp)(σ: Store, ρ: Env) = σ
+  def extendWithCond(test: Exp, σ: Store, ρ: Env, dir: Boolean) = σ
+
 }
